1b37153ebf28f32680ea7f07b05c312c11f53314
TEILLAIS Johan <johan.teillais@ynov.com>
2018-12-06 10:13:32 +0100

    Debut Projet Empty Activity create

---
+++ app/src/main/java/com/example/utilisateur/projetmadrental/MadRentalActivity.java
@@ -0,0 +1,13 @@
+package com.example.utilisateur.projetmadrental;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MadRentalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mad_rental);
+    }
+}
---
+++ app/src/main/java/com/example/utilisateur/projetmadrental/ReservationEtape2.java
@@ -0,0 +1,13 @@
+package com.example.utilisateur.projetmadrental;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReservationEtape2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_etape2);
+    }
+}
