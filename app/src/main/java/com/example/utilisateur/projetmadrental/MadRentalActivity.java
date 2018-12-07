3606eff20c1cd721e385a0799a6c3e97b27177c8
Rigaudeau Léo <leo.rigaudeau@ynov.com>
2018-12-07 11:27:22 +0100

    reduction doublon

--- app/src/main/java/com/example/utilisateur/projetmadrental/MadRentalActivity.java
+++ app/src/main/java/com/example/utilisateur/projetmadrental/MadRentalActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
 
 public class MadRentalActivity extends AppCompatActivity {
