d3880fc56f7293f1b58512f326558e62f0c3b004
Rigaudeau Léo <leo.rigaudeau@ynov.com>
2018-12-18 11:42:46 +0100

    ajout jo

--- app/src/main/java/com/example/utilisateur/projetmadrental/VehiculeViewHolder.java
+++ app/src/main/java/com/example/utilisateur/projetmadrental/VehiculeViewHolder.java
@@ -2,16 +2,26 @@ package com.example.utilisateur.projetmadrental;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class VehiculeViewHolder extends RecyclerView.ViewHolder {
     // TextView intitulé course :
     public TextView textViewLibellenom;
+    public TextView textViewLibelleprix;
+    public TextView textViewLibelleCO2;
+    public TextView textViewLibellePromotion;
+    public ImageView textViewLibelleimage;
     // Constructeur :
 
     public VehiculeViewHolder(View itemView)
     {
         super(itemView);
         textViewLibellenom = itemView.findViewById(R.id.libelle_vehicule);
+        textViewLibelleprix = itemView.findViewById(R.id.prix_vehicule);
+        textViewLibelleCO2 = itemView.findViewById(R.id.co2_vehicule);
+        textViewLibellePromotion = itemView.findViewById(R.id.Promotion_vehicule);
+        textViewLibelleimage = itemView.findViewById(R.id.image_vehicule);
+
     }
 }
